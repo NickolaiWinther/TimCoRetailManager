959c3f0698baa58433d47ad782ee229bc82e6305
Nickolai Schioldan Winther <nick3846@edu.campusvejle.dk>
2020-06-15 09:37:37 +0200

    Update EF Databse

--- TRMData/dbo/Stored Procedures/spProduct_GetAll.sql
+++ TRMData/dbo/Stored Procedures/spProduct_GetAll.sql
@@ -3,7 +3,7 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	SELECT id, ProductName, [Description], RetailPrice, QuantityInStock, IsTaxable 
+	SELECT Id, ProductName, [Description], RetailPrice, QuantityInStock, IsTaxable 
 	FROM dbo.Product
 	ORDER BY ProductName
 END 
